0bb08e158e3e6ebb3240b966b1a5f3d9b83bb262
gruzzilkin <gruzzilkin@gmail.com>
2022-07-03 21:50:17 +0500

    Шаг 3: сторонние библиотеки.

---
+++ src/cli.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+import readlineSync from 'readline-sync';
+
+export default () => {
+  const name = readlineSync.question('May I have your name? ');
+
+  console.log(`Hello, ${name}!`);
+
+  return name;
+};
